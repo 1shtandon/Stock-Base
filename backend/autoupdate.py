ee2fcd90c06d254d9c80b3a424d054af41ee1b2f
Anubhav Kushwaha <k04kushwaha@gmail.com>
2022-04-28 23:44:50 +0530

    final

---
+++ backend/autoupdate.py
@@ -0,0 +1,7 @@
+import os
+import time
+
+while True:
+    os.system("python commands.py --command update")
+    time.sleep(10)
+
--- backend/test.py
+++ backend/test.py
@@ -1,20 +1,21 @@
-import os
+# import os
+#
+# import mysql.connector
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# connection = mysql.connector.connect(
+#     host=os.environ['MYSQL_HOST'],
+#     user=os.environ['MYSQL_USER'],
+#     password=os.environ['MYSQL_PASSWORD'],
+#     database=os.environ['MYSQL_DATABASE']
+# )
+#
+# cursor = connection.cursor()
+# cursor.execute("SELECT * FROM user_table")
+# print(cursor.fetchone())
+# print(cursor.fetchone())
+# print(cursor.fetchone())
 
-import mysql.connector
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-connection = mysql.connector.connect(
-    host=os.environ['MYSQL_HOST'],
-    user=os.environ['MYSQL_USER'],
-    password=os.environ['MYSQL_PASSWORD'],
-    database=os.environ['MYSQL_DATABASE']
-)
-
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM user_table")
-print(cursor.fetchone())
-print(cursor.fetchone())
-print(cursor.fetchone())
